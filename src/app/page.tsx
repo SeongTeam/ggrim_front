9b4fb577dd6e362120d5a6cbbbc98d58d9a6ad94
Cori1304-Seong <andyjm7739@gmail.com>
2024-11-21 18:37:14 +0900

    feat:  메인 layout& page에 recoil 적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,16 @@
-// components
 import { Navbar, Footer } from "@/components";
 import { ArtworkCarousel } from "./home/artwork_carousel";
 import Gallery from "@/components/coverflowGallery";
 import type { ImgData } from "@/types";
 import { ArtworkQuiz } from "./home/artwork_quiz";
-
-// sections
+import { MackRecoilUI } from "../mock/mock_recoil";
 
 export default function Campaign() {
   return (
     <>
       <ArtworkCarousel />
       <ArtworkQuiz />
+      <MackRecoilUI></MackRecoilUI>
       {/* <Footer /> */}
     </>
   );
