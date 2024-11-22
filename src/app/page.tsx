8ae7dd72dfda587d4bac94e87b6f17ba40cd89a6
Cori1304-Seong <andyjm7739@gmail.com>
2024-11-22 16:49:28 +0900

    refactor: 보여 줄 필요 없는 MackRecoilUI 주석

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Campaign() {
     <>
       <ArtworkCarousel />
       <ArtworkQuiz />
-      <MackRecoilUI></MackRecoilUI>
+      {/* <MackRecoilUI></MackRecoilUI> */}
       {/* <Footer /> */}
     </>
   );
