0df012ad8ad419a8fc027241129382e8fa6720d0
Cori1304-Seong <andyjm7739@gmail.com>
2024-11-25 17:12:33 +0900

    feat: 이미지 MCQ UI 추가

--- src/components/extensions/mcq/reader/MCQ_reader_view.tsx
+++ src/components/extensions/mcq/reader/MCQ_reader_view.tsx
@@ -9,13 +9,8 @@ import SubmissionFeedback from "./parts/submission_feedback";
 import { MCQReaderViewProps } from "@/types/mcq_types";
 
 const MCQReaderView = ({ attrs }: MCQReaderViewProps) => {
-  const {
-    question,
-    displayAnswers: answers,
-    selectedAnswer,
-    id,
-    showHintButton,
-  } = attrs;
+  const { question, displayAnswers, selectedAnswer, id, showHintButton } =
+    attrs;
 
   const {
     errorMessage,
@@ -31,24 +26,38 @@ const MCQReaderView = ({ attrs }: MCQReaderViewProps) => {
   } = useMCQReader(attrs);
 
   return (
-    <div className="p-4 rounded-md shadow bg-ggrimBeige2 ">
+    <div className="p-4 rounded-md shadow bg-ggrimBeige2">
       <h3 className="text-xl font-bold text-gray-800 mb-6">{question}</h3>
       {errorMessage && <ErrorMessage message={errorMessage} />}
-      <ul className="my-2 pl-8">
-        {answers.map((answer, index) => (
-          <li key={index} className="flex items-center gab-4 mb-2">
-            <input
-              type="radio"
-              name={`mcq-reader-${id}-${index}`}
-              checked={readerSelectedAnswer === index}
-              onChange={() => handleReaderSelectAnswer(index)}
-              className="h-5 w-5 radio radio-primary"
-              disabled={isSubmitted && attemptedAnswers.includes(index)}
-            />
-            <div className="mx-2"></div>
-            <span className="text-base font-medium text-gray-700">
-              {answer}
-            </span>
+
+      <div className="grid md:grid-cols-2 gap-4 sm:grid-cols-1">
+        {displayAnswers.map((answer, index) => (
+          <div
+            key={index}
+            className="flex flex-col items-center p-4 rounded-md bg-white shadow-md"
+          >
+            <div className="flex flex-col items-start gap-4">
+              <div className="flex flex-row items-start gap-2">
+                <input
+                  type="radio"
+                  name={`mcq-reader-${id}-${index}`}
+                  checked={readerSelectedAnswer === index}
+                  onChange={() => handleReaderSelectAnswer(index)}
+                  className="h-5 w-5 radio radio-primary"
+                  disabled={isSubmitted && attemptedAnswers.includes(index)}
+                />
+                <span className="text-base font-medium text-gray-700">
+                  {answer}
+                </span>
+              </div>
+
+              <img
+                src="https://imagescdn.gettyimagesbank.com/500/202311/jv13117838.jpg"
+                alt={`Answer ${index}`}
+                className="w-50 h-auto rounded-md mb-2"
+              />
+            </div>
+
             {isSubmitted && isCorrect && index === selectedAnswer && (
               <Icons.CircleCheck
                 className="w-6 h-6 text-success ml-2"
@@ -60,32 +69,9 @@ const MCQReaderView = ({ attrs }: MCQReaderViewProps) => {
               index !== selectedAnswer && (
                 <Icons.CircleX className="w-6 h-6 text-error ml-2" fill="red" />
               )}
-          </li>
+          </div>
         ))}
-      </ul>
-      <div className="flex justify-between items-center">
-        <SubmissionFeedback
-          isCorrect={isCorrect}
-          isSubmitted={isSubmitted}
-          readerSelectedAnswer={readerSelectedAnswer}
-          handleSubmit={handleSubmit}
-          handleHintButtonClick={handleHintButtonClick}
-          handleClearSubmission={handleClearSubmission}
-          showHintButton={showHintButton ?? true}
-        />
       </div>
-      {showHint && (
-        <HintPane
-          mcqId={id}
-          show={showHint}
-          onClose={handleHintButtonClick}
-          question={question}
-          attemptedAnswers={attemptedAnswers.map((index) => answers[index])}
-          remainingAnswers={answers.filter(
-            (_, index) => !attemptedAnswers.includes(index)
-          )}
-        />
-      )}
     </div>
   );
 };
