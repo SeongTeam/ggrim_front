a5c81da6ed9c61c915ea83774aa0fc324d34bc01
Cori1304-Seong <andyjm7739@gmail.com>
2024-12-18 17:12:33 +0900

    feat: Add change video Error msg logic

--- src/app/watch/components/videoPlayer.tsx
+++ src/app/watch/components/videoPlayer.tsx
@@ -3,20 +3,61 @@
 import { CldVideoPlayer } from 'next-cloudinary';
 import 'next-cloudinary/dist/cld-video-player.css';
 import { useRouter } from 'next/router';
+import { useSearchParams } from 'next/navigation';
+import CaesarCipher from '@/util/caesarCipher';
+import { useEffect, useState } from 'react';
 
 const VideoPlayer = () => {
-    const src: string = 'On_a_throne_of_velvet_he_sits_all_alone_dwvwtl';
+    const caesar = new CaesarCipher();
+    const searchParams = useSearchParams();
+    const search = searchParams.get('message');
 
-    // const router = useRouter();
-    // const { message } = router.query;
+    const [errorMessage, setErrorMessage] = useState<string | null>(null);
+
+    // 에러 처리 함수
+
+    // let src = caesar.decode(search ?? '');
+    let src = '12324';
+
+    // const VideoMessage = () => {
+    //     useEffect(() => {
+    //         const element = document.querySelector('.vjs-modal-dialog-content');
+    //         if (element) {
+    //             element.textContent = 'Updated message from DOM manipulation';
+    //         }
+    //     }, []);
+
+    //     return null; // React 방식이 아닌 경우 렌더링 요소가 필요 없습니다.
+    // };
+
+    const VideoMessage = () => {
+        const element = document.querySelector('.vjs-modal-dialog-content');
+        if (element) {
+            element.textContent = 'Video cannot be played. Please try again later.';
+        }
+    };
+
     return (
         <CldVideoPlayer
             id="mock_cldVideoo"
-            width="412" //412 × 732
+            width="412"
             height="732"
-            src={src}
+            src={src ?? ''}
+            onError={(e: any) => {
+                VideoMessage(); // DOM 조작
+            }}
         />
     );
 };
 
 export default VideoPlayer;
+
+// if (e.Player.videojs.error_) {
+//     let title = document.querySelector('.error-container');
+//     title!.innerHTML =
+//         'Houston, we have a problem: ' +
+//         e.Player.videojs.error_.message +
+//         '. This is the status code: ' +
+//         e.Player.videojs.error_.statusCode;
+//     title!.style.color = 'red';
+// }
