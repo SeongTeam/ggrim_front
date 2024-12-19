f6ca5c65086b626b062c276fdeb1539119c5011f
Cori1304-Seong <andyjm7739@gmail.com>
2024-12-19 16:20:19 +0900

    refactor: del unused code on route.tsx files

--- src/app/api/artwork_week/route.ts
+++ src/app/api/artwork_week/route.ts
@@ -1,8 +1,8 @@
-import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
 import { promises as fs } from 'node:fs';
 
-export async function GET(req: NextRequest) {
+// export async function GET(req: NextRequest) {
+export async function GET() {
     const path =
         __dirname.replace('.next/server/app/api/artwork_week', 'src/app') +
         '/data/artwork_week-paintings.json'; //.next/server is the path to the build folder that is created when you run next command
