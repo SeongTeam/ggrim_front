a15b22f99ba4940f89fe475a733db73ff03d0e50
Cori1304 <160833798+Cori1304-Seong@users.noreply.github.com>
2024-12-28 14:21:01 +0900

    Fix/deploy buill error (#9)
    * feat: move dockerfile env setting
    
    * feat: add BACKEND_URL default
    
    * feat: set HOSTNAME env on dockerfile
    
    * feat: change BACKEND_URL default value
    
    * feat: Add ARG command on dockerfile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,10 @@ import { Painting } from '@/mock/data/entity/mock_painting';
 import { MCQAttribute } from '@/types/mcq_types';
 import { CuratedWorkAttribute } from '@/types/curatedArtwork-types';
 
-const BACKEND_URL = process.env.BACKEND_URL;
+// TODO 배포할때 더 좋은 방법이 있을지 생각해보기
+const BACKEND_URL =
+    process.env.BACKEND_URL ||
+    'https://port-0-grim-dev-nest-server-m4i5o7t86f50fb45.sel4.cloudtype.app';
 
 // TODO page.tsx 최소화 예정 (데이터 처리 함수 옮길 예정)
 // TODO 함수 분리 예정
