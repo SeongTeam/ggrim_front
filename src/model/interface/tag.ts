6b59d03289656098a63a75bdc1a719ce409ac32f
Cori1304-Seong <andyjm7739@gmail.com>
2025-01-07 15:39:08 +0900

    feat:  Add interface Tag

---
+++ src/model/interface/tag.ts
@@ -0,0 +1,9 @@
+export interface Tag {
+    created_date: string;
+    updated_date: string;
+    deleted_date: string | null;
+    version: number | null;
+    id: string;
+    name: string;
+    info_url: string | null;
+}
