eb3e3e393ce1557a92fb413bcee06c0108761436
Cori1304-Seong <andyjm7739@gmail.com>
2025-01-23 16:28:56 +0900

    feat: Add getUrlImageSize on imageUtils.ts
    이미지의 width와 height 데이터 저장과Hover ImagePreview에서 사용

---
+++ src/util/imageUtiles.ts
@@ -0,0 +1,17 @@
+/**
+ * 이미지 URL을 집어 넣으면 그 이미지의 너비와 높이를 반환
+ * @param 이미지URL
+ * @returns  width:number, height:number
+ */
+export const getUrlImageSize = (url: string): Promise<{ width: number; height: number }> => {
+    return new Promise((resolve) => {
+        const img = new Image();
+        img.onload = () => {
+            resolve({ width: img.naturalWidth, height: img.naturalHeight });
+        };
+        img.onerror = () => {
+            resolve({ width: 0, height: 0 }); // 로드 실패 시 기본값
+        };
+        img.src = url;
+    });
+};
