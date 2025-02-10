6788e12a1a6c6ffc7e187a8f2581297836a0c63a
Cori1304-Seong <andyjm7739@gmail.com>
2025-02-10 14:53:08 +0900

    feat: Add BACKEND_URL read log logic

---
+++ src/app/lib/apis.ts
@@ -0,0 +1,43 @@
+'use server';
+
+import { MCQAttribute } from '@/model/interface/MCQ';
+import { CuratedArtWorkAttribute } from '@/model/interface/curatedArtwork-types';
+
+function getServerUrl(): string {
+    const url = process.env.BACKEND_URL;
+    console.log(`$$$$ getServerUrl() ==>  ${url} $$$$`);
+
+    if (url == undefined) {
+        console.error(` 'process.env.BACKEND_URL' not read`);
+        return '';
+    }
+    return url;
+}
+// TODO page.tsx 최소화 예정 (데이터 처리 함수 옮길 예정)
+export const getWeekArtWorkData = async (): Promise<CuratedArtWorkAttribute[]> => {
+    // const response = await fetch('http://localhost:4000/api/artwork_week', {
+    //     cache: 'no-cache',
+    // });  // src/data에 파일을 읽어 올 때 사용
+    const serverUrl = getServerUrl();
+
+    const url: string = serverUrl + '/painting/artwork_of_week';
+    const response = await fetch(url, {
+        cache: 'no-cache',
+    }); // 서버에 있는 데이터 읽어 올때 사용
+    const res = await response.json();
+    return res.data;
+};
+
+export const getMCQData = async (): Promise<MCQAttribute[]> => {
+    // const response = await fetch('http://localhost:4000/api/mcq', {
+    //     cache: 'no-cache',
+    // });  // src/data에 파일을 읽어 올 때 사용
+    const serverUrl = getServerUrl();
+
+    const url: string = serverUrl + '/quiz/quiz_of_week';
+    const response = await fetch(url, {
+        cache: 'no-cache',
+    }); // 서버에 있는 데이터 읽어 올때 사용
+    const res = await response.json();
+    return res.data;
+};
