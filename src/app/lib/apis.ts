47d09fbf8e42c1f08254ec114e1971d5eaf1b425
armcortex3445 <wlghks5966@naver.com>
2025-02-12 15:53:34 +0900

    feat(api) : add search painting api
    - provide logic searching painting from backend

--- src/app/lib/apis.ts
+++ src/app/lib/apis.ts
@@ -3,6 +3,7 @@
 import { MCQAttribute } from '@/model/interface/MCQ';
 import { CuratedArtWorkAttribute } from '@/model/interface/curatedArtwork-types';
 import { serverLogger } from '@/util/logger';
+import { Painting } from '../../model/interface/painting';
 
 function getServerUrl(): string {
     const url = process.env.BACKEND_URL;
@@ -41,4 +42,21 @@ export const getMCQData = async (): Promise<MCQAttribute[]> => {
     }); // 서버에 있는 데이터 읽어 올때 사용
     const res = await response.json();
     return res.data;
+};
+
+export const findPainting = async (
+    title: string = '',
+    artist: string = '',
+    tags: string[] = [],
+    styles: string[] = [],
+): Promise<Painting[]> => {
+    const backendUrl = getServerUrl();
+    const response = await fetch(
+        `${backendUrl}/painting?title=${title}&artistName=${artist}&tags=${JSON.stringify(
+            tags,
+        )}&styles=${JSON.stringify(styles)}`,
+    );
+    const result = await response.json();
+    const paintings: Painting[] = result.data as Painting[];
+    return paintings;
 };
