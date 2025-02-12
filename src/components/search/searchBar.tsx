a558e76b4023026658bbab261cb9bb694e00b241
armcortex3445 <wlghks5966@naver.com>
2025-02-12 18:20:08 +0900

    feat(component) : add logic to SearchBar component
    - provide logic showing default input value

--- src/components/search/searchBar.tsx
+++ src/components/search/searchBar.tsx
@@ -1,13 +1,15 @@
+'use client'
 import { Search } from "lucide-react";
 import React, { useEffect, useState } from "react";
 
 
 interface SearchBarProps {
   onSearch: (query: string) => void;
+  defaultValue? : string;
 }
-export function SearchBar({ onSearch }: SearchBarProps) {
+export function SearchBar({ onSearch, defaultValue }: SearchBarProps) {
 
-  const [inputValue, setInputValue] = useState("");
+  const [inputValue, setInputValue] = useState(defaultValue||"");
 
   const DELAY_MS = 500;
   useEffect(() => {
@@ -17,11 +19,14 @@ export function SearchBar({ onSearch }: SearchBarProps) {
 
     return () => clearTimeout(delayDebounce);
   }, [inputValue, onSearch]);
+  
+
 
   return (
     <div className="relative">
       <Search className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400" size={20} />
       <input
+        value={inputValue}
         type="text"
         placeholder="Search Title..."
         onChange={(e) => setInputValue(e.target.value)}
