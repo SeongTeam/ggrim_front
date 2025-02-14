1ee89472222fb43f991a1dee9920e3ecee8dbb88
armcortex3445 <wlghks5966@naver.com>
2025-02-14 13:59:19 +0900

    feat(app) : provide detail painting modal
    - provide modal for detail painting info by clicking

--- src/components/search/PaintingCardGrid.tsx
+++ src/components/search/PaintingCardGrid.tsx
@@ -1,6 +1,11 @@
+'use client';
+import { useState } from "react";
 import { Painting } from "../../model/interface/painting";
 import HoverCard from "../HoverCard";
 import { PreviewPainting } from '../PreviewPainting';
+import { Modal } from "../Modal";
+import { PaintingDetailView } from "../PaintingDetailView";
+import { getPainting } from "../../app/lib/apis";
 
 interface PaintingCardGridProps  {
     paintings : Painting[];
@@ -8,13 +13,34 @@ interface PaintingCardGridProps  {
 
 export function PaintingCardGrid({ paintings } : PaintingCardGridProps ): React.JSX.Element {
 
+    const [selectedPainting , setSelectedPainting] = useState<Painting|undefined>(undefined);
+
+    const openModal = async (paintingId : string) =>{
+        const painting = await getPainting(paintingId)
+        setSelectedPainting(painting);
+    }
+
+    const closeModal = () =>{
+        setSelectedPainting(undefined);
+    }
+
+
+
     return (
         <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 mt-4">
                 {paintings.map((item) => (
-                    <HoverCard key={`${item.id}+HoverCardItem`} cardProps ={{imageSrc : item.image_url, alt : item.title, title : item.title}}>
+                    <HoverCard key={`${item.id}+HoverCardItem`} 
+                        cardProps ={{imageSrc : item.image_url, alt : item.title, title : item.title}}
+                        onClick={()=>openModal(item.id)}
+                        >
                         <PreviewPainting painting={item} />
                     </HoverCard>
                 ))}
+                {selectedPainting &&
+                <Modal onClose={closeModal}>
+                    <PaintingDetailView painting={selectedPainting}/>
+                </Modal>
+                }
         </div>
     );
 }
