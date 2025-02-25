3f42f5c7a61f3eb32574501d62cfa8b24f44ed86
armcortex3445 <wlghks5966@naver.com>
2025-02-25 16:22:20 +0900

    fix(component) : change component to <Image/>
    - follow eslint rule @next/next/no-img-element
    - apply <Image/> on component where height and width of source exist
    
    Breaking : change <img/> to <Image/>
    
    Before {
     use <img/> even though height and width of source exist
    }
    
    After {
     use <Image/> when height and width of source exist
    }

--- src/components/PreviewPainting.tsx
+++ src/components/PreviewPainting.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { Painting } from '../model/interface/painting';
+import Image from 'next/image';
 
 interface PreviewPaintingProps {
     painting: Painting;
@@ -11,7 +12,7 @@ export const PreviewPainting = ({ painting }: PreviewPaintingProps) => {
     return (
         <div className="absolute bottom-0 right-0 mt-8 bg-gray-900 shadow-lg border-0 rounded-lg  z-10">
             <div className="max-h-64 rounded">
-                <img src={painting.image_url} alt="Preview" className="w-full object-cover rounded" />
+                <Image height={painting.height} width={painting.width} src={painting.image_url} alt="Preview" className="w-full object-cover rounded" />
             </div>
             <div className='p-2'>
                 <h3 className='mb-8 text-white'>
--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,15 +1,17 @@
-
+import Image from "next/image";
 export interface CardProps {
     imageSrc : string,
     alt : string,
     title : string,
+    height? : number,
+    width? : number,
   }
   
-export function Card({ imageSrc,alt,title }: CardProps) {
+export function Card({ imageSrc,alt,title,height,width }: CardProps) {
     return (
 
           <div className="bg-gray-900 rounded-lg overflow-hidden">
-            <img src={imageSrc} alt={alt} className="w-full h-40 object-cover" />
+            <Image height={height} width={width} src={imageSrc} alt={alt} className="w-full h-40 object-cover" />
             <div className="p-2">
               <p className="text-white text-sm">{title}</p>
             </div>
