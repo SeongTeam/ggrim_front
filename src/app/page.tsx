6b01f0724c2131fc24dddcc7d732b23c2295c400
armcortex3445 <wlghks5966@naver.com>
2025-03-04 17:09:17 +0900

    feat(app) : update main page
    - change providing quiz UI to navigating quiz page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,26 @@
 // import { Navbar } from '@/components';
 import { ArtworkCarousel } from '../components/home/artworkCarousel';
-import { ArtworkQuiz } from '../components/home/artworkQuiz';
-import { MCQAttribute } from '@/model/interface/MCQ';
 import { CuratedArtWorkAttribute } from '@/model/interface/curatedArtwork-types';
-import { getMCQData, getWeekArtWorkData } from '@/app/lib/api.backend';
+import { getWeekArtWorkData } from '@/app/lib/api.backend';
+import ScrollTriggerNavigator from '../components/quiz/ScrollTriggerNavigator';
+import { getQuizIDByContext } from './lib/api.quiz';
+import { Quiz } from '../model/interface/quiz';
 
 // TODO 배포할때 더 좋은 방법이 있을지 생각해보기
 
 // TODO 함수 이름 변경 예정
 export default async function Campaign() {
-    const quizOfWeekData: MCQAttribute[] = await getMCQData();
     const artworkOfWeekData: CuratedArtWorkAttribute[] = await getWeekArtWorkData();
-
+    const domID = `main`;
+    const quiz : Quiz = (await getQuizIDByContext()).quiz;
     return (
-        <>
+
+        <div id={domID}>
             {/* <Navbar /> */}
             <ArtworkCarousel curatedWorkAttributes={artworkOfWeekData} />
-            <ArtworkQuiz mcqAttributes={quizOfWeekData} />
             {/* <MackRecoilUI></MackRecoilUI> */}
             {/* <Footer /> */}
-        </>
+            <ScrollTriggerNavigator section={{id : domID, path : `/quiz/${quiz.id}` }} />
+            </div>
     );
 }
