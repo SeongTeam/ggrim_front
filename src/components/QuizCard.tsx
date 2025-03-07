dc5a7512458be292a92d10172de600f3ec6437ab
armcortex3445 <wlghks5966@naver.com>
2025-03-07 17:34:16 +0900

    feat(component) : add <QuizCard/>
    - provide overview QUiz

---
+++ src/components/QuizCard.tsx
@@ -0,0 +1,32 @@
+
+export interface QuizCardProps {
+    title : string,
+    onClick : ()=>void
+  }
+
+      // TODO: <QuizCard /> UI 개선
+    // - [ ] 사용자에게 보여줄 정보 추가
+    // ! 주의: <경고할 사항>
+    // ? 질문: <의문점 또는 개선 방향>
+    // * 참고: <관련 정보나 링크>
+  
+export function QuizCard({ title, onClick}: QuizCardProps) {
+    const size = 20;
+    const uiTitle = title.length > size ? title.slice(0,size) + '...' : title;
+    return (
+
+          <div className="bg-gray-900 rounded-lg overflow-hidden  hover:bg-gray-700" >
+            <div className="bg-white p-2 pt-4 rounded-lg h-24 ">
+              <p className="text-gray-700 text-lg font-bold font-sans-serif">{uiTitle}</p>
+            </div>
+
+            <div className="m-3 ">
+            <p className="inline-block text-slate-200 border-b-2 border-transparent hover:border-slate-200 hover:border-b-2 transition-all cursor-pointer"
+              onClick={onClick}
+            >
+              Play
+            </p>
+            </div>
+          </div>
+        );
+  }
