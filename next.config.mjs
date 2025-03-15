54b6a7dff646ece46641458bf3a7d6a0421404b4
armcortex3445 <wlghks5966@naver.com>
2025-03-15 17:21:58 +0900

    chore(app) : config vscode nextjs debug

--- next.config.mjs
+++ next.config.mjs
@@ -11,6 +11,14 @@ const nextConfig = {
             },
         ],
     },
+    // solve debug console error that un file '/.next/server/vendor-chunks/render-from-template-context.js.map'
+    //ref : https://github.com/vercel/next.js/issues/65795#issuecomment-2439641016 
+    webpack: (config, { dev, isServer }) => {
+        if (dev) {
+          config.devtool = 'eval-source-map'
+        }
+        return config
+      },
 };
 
 export default nextConfig;
