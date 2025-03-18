88569eb0d164c27a126759107418aebb7bfa1daa
armcortex3445 <wlghks5966@naver.com>
2025-03-18 10:35:15 +0900

    feat(components) : improve UI
    - use <Link> to prefetch route
    - increase text size for user to be comfortable

--- src/components/NavbarMenu.tsx
+++ src/components/NavbarMenu.tsx
@@ -1,25 +1,19 @@
 "use client"
 import { useState } from "react";
 import { Menu, X } from "lucide-react";
-import { useRouter } from "next/navigation";
+import Link from "next/link";
+
+const NavRoute = {
+ home : '/',
+ quiz : '/quiz',
+ quizCreate : '/quiz/create'
+ 
+}
 
 export default function NavbarMenu() {
   const [isOpen, setIsOpen] = useState(false);
-  const router = useRouter();
 
   const toggleMenu = () => setIsOpen((prev) => !prev);
-
-  const handleClickHome = ()=>{
-    router.push('/');
-  }
-
-  const handleClickQuiz = ()=>{
-    router.push('/quiz');
-  }
-
-  const handleClickCreateQuiz = () =>{
-    router.push('/quiz/create');
-  }
 
   return (
     <div className="relative flex items-center justify-center">
@@ -33,19 +27,19 @@ export default function NavbarMenu() {
 
       {/* 네비게이션 메뉴 */}
       <ul
-        className={`absolute md:relative top-full left-0 w-full min-w-48 md:w-auto bg-gray-900 md:bg-transparent p-4 md:p-0 flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-6 text-white text-sm ${
+        className={`absolute md:relative top-full left-0 w-full min-w-48 md:w-auto bg-gray-900 md:bg-transparent p-4 md:p-0 flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-6 text-white text-base ${
           isOpen ? "block" : "hidden md:flex"
         }`
         }
       >
-        <li className="hover:text-gray-300 cursor-pointer" onClick={() => handleClickHome()}>
-          Home
+        <li className="hover:text-gray-300 cursor-pointer">
+          <Link href={NavRoute.home}> Home</Link>
         </li>
-        <li className="hover:text-gray-300 cursor-pointer" onClick={() => handleClickQuiz()}>
-          Quiz
+        <li className="hover:text-gray-300 cursor-pointer">
+          <Link href={NavRoute.quiz}> Quiz</Link>
         </li>
-        <li className="hover:text-gray-300 cursor-pointer" onClick={() => handleClickCreateQuiz()}>
-          Create Quiz
+        <li className="hover:text-gray-300 cursor-pointer" >
+        <Link href={NavRoute.quizCreate}> Create Quiz</Link>
         </li>
       </ul>
     </div>
