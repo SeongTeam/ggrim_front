e67c99cd35288a2aff8e5a02a070260765febdf1
armcortex3445 <wlghks5966@naver.com>
2025-05-02 11:13:55 +0900

    feat(model) : add user interface
    - update interface to be consistent with Backend

---
+++ src/model/interface/user.ts
@@ -0,0 +1,15 @@
+export type UserRole = 'admin' | 'user';
+
+export type UserState = 'active' | 'inactive' | 'banned';
+export interface User {
+    id: string;
+    email: string;
+
+    role: UserRole;
+
+    username: string;
+
+    active: UserState;
+
+    last_login_date: Date;
+}
