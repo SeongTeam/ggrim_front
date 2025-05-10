9bd6d1ca822f5327ded2d810b6db2375a6a09b59
armcortex3445 <wlghks5966@naver.com>
2025-05-10 19:49:55 +0900

    feat(app) : add page
    - provide page to update password

---
+++ src/app/auth/update-password/page.tsx
@@ -0,0 +1,42 @@
+import { PasswordUpdateForm } from "../../../components/auth/PasswordUpdateForm";
+import { updateUserPWAction } from "../../../server-action/backend/user/api";
+
+
+interface ForgetPasswordProps {
+  searchParams : Promise<{ [key: string] : string | string[] | undefined }>
+}
+
+//TODO : 로그인 페이지 개선
+// - [ ] 회원가입 및 비밀번호 찾기 라우팅 로직 적용하기
+export default async function UpdatePassword({
+} : ForgetPasswordProps) {
+
+  const createAccountRoute = '';
+  const forgotPasswordRoute = '';
+
+  const handleAction = async (password : string) => {
+    'use server'
+      return updateUserPWAction({password});
+  }
+
+
+  return (
+    <main className="flex items-center justify-center min-h-screen bg-cover bg-center">
+      <div className="bg-black bg-opacity-75 p-8 rounded-md w-full max-w-md">
+        <h1 className="text-3xl font-bold mb-6">Sign In</h1>
+        <PasswordUpdateForm 
+          formAction={handleAction}
+          NextRoute={'/'} 
+        />
+        <div className="flex justify-between">
+          <p className="mt-4 text-sm text-gray-400">
+            <a href={createAccountRoute} className="text-white hover:underline">Create Account</a>
+          </p>
+          <p className="mt-4 text-sm text-gray-400">
+            <a href={forgotPasswordRoute} className="text-white hover:underline">Forgot Password</a>
+          </p>
+        </div>
+      </div>
+    </main>
+  )
+}
