d8aadc91f1bc947b7f1b6855574f09e80b9988f1
armcortex3445 <wlghks5966@naver.com>
2025-05-14 13:21:44 +0900

    doc(server-action) : add TODO

--- src/server-action/backend/cookie.ts
+++ src/server-action/backend/cookie.ts
@@ -27,6 +27,13 @@ export async function getSignInResponseOrRedirect(): Promise<SignInResponse> {
     const signInResponse = cookieStore.get(ENUM_COOKIE_KEY.SIGN_IN_RESPONSE)?.value;
 
     if (!signInResponse) {
+        //TODO 쿠키 만료 핸들 로직 개선
+        // - [ ] : 페이지 이동을 클라이언트가 담당하도록 하기
+        //  -> redirect 사용시 2가지 문제 존재
+        //      1. 서버 컴포넌트 재렌더링 불가(layout 내의 <NavBar/>가 렌더링되지 않음)
+        //      2. 클라이언트 측의 로직 예상 어려움
+        //      - 개선 방법으로는, server-action 에러를 정의하여, 클라이언트에서 핸들링하도록 하기.
+        // revalidatePath('/', 'layout');
         redirect(AUTH_LOGIC_ROUTE.SIGN_IN);
         // return undefined;
     }
