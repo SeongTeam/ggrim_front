b4ee4de9fc361d1cf405f756e73a90c685732558
armcortex3445 <wlghks5966@naver.com>
2025-05-14 13:37:12 +0900

    feat(app) : check authentication when create quiz

--- src/app/quiz/create/page.tsx
+++ src/app/quiz/create/page.tsx
@@ -1,10 +1,18 @@
+import ErrorModal from "../../../components/ErrorModal";
 import QuizForm from "../../../components/quiz/QuizForm";
+import { getSignInInfo } from "../../../server-action/backend/cookie";
 
-export default function QuizCreatePage(){
+export default async function QuizCreatePage(){
+
+    const user = await getSignInInfo();
+
+    if(!user){
+        return <ErrorModal message="Need to Sign In" />
+    }
 
     return(
     <div className="mt-10">
-     <QuizForm />
+        <QuizForm />
      </div>
     );
 }
