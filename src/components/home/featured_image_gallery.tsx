dd2c69bfbb21e2d2ffaf958afcf1a3f7338fda9d
armcortex3445 <wlghks5966@naver.com>
2025-05-27 16:41:36 +0900

    feat(components) : redesign component
    - make component simple
    - reuse <Modal/> and <PaintingDetailView/> to integrate UI
    - use <Image /> instead of <img /> to optimize image UI

--- src/components/home/featured_image_gallery.tsx
+++ src/components/home/featured_image_gallery.tsx
@@ -2,6 +2,9 @@
 
 import React, { useState} from 'react';
 import { Painting } from '../../model/interface/painting';
+import Image from 'next/image';
+import { PaintingDetailView } from '../PaintingDetailView';
+import { Modal } from '../modal/Modal';
 
 export type FeaturedImageGalleryProps = {
     paintings: Painting[];
@@ -26,64 +29,43 @@ export const FeaturedImageGallery: React.FC<FeaturedImageGalleryProps> = ({ pain
     };
 
     return (
-        <div className="flex flex-col md:flex-row items-start gap-5 px-4 py-6 max-w-4xl mx-auto min-h-[600px]">
-            {/* Main Image Carousel */}
-            <div className="w-full md:w-7/12 min-h-[450px] relative flex justify-center items-center bg-gray-200">
-                {/* <img
-                    src={currentImage}
-                    alt="Main Image"
-                    className="object-contain max-h-[850px] max-w-full  p-1 "
-                /> */}
-                <section
-                    onClick={handleOpen}
-                    className="relative flex items-center justify-center  min-h-64 max-h-[850px] max-w-full p-1"
-                >
-                    <img src={painting.image_url} alt="Main Image" />
-                </section>
-                {/* 왼쪽 버튼 */}
-                <button
-                    onClick={goToPrevious}
-                    className="absolute left-4 top-1/2 transform -translate-y-1/2 bg-gray-800 text-white p-2 rounded-full"
-                >
-                    ◀
-                </button>
-                {/* 오른쪽 버튼 */}
-                <button
-                    onClick={goToNext}
-                    className="absolute right-4 top-1/2 transform -translate-y-1/2 bg-gray-800 text-white p-2 rounded-full"
-                >
-                    ▶
-                </button>
-            </div>
-            {/* Thumbnail Navigation */}
-            <div className="flex flex-col w-full md:w-1/2 bg-yellow-400">
-                <h2 className="text-xl font-bold text-gray-800 mb-2">PAINTING OF THE WEEK</h2>
-                <h3 className="text-2xl font-semibold text-gray-900"> {painting.title}</h3>
-                <p className="text-blue-600 text-sm mb-2">{painting.artist.name}</p>
-                <p className="text-gray-700 text-sm mb-4">
-                    {painting.description}
-                </p>
-            </div>
+        <div className="flex flex-col ml-5 pt-5 mb-2 justify-start">
 
-            {/* Fullscreen Modal */}
-            {isOpen && (
-                <div
-                    className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-75 z-50"
-                    onClick={handleClose}
-                >
-                    <img
-                        src={painting.image_url}
-                        alt="mock full"
-                        className="max-w-full max-h-full object-contain"
-                    />
+            <section className="flex flex-col mr-auto items-start gap-5 pt-6 pb-10 max-w-4xl ">
+                {/* Main Image Carousel */}
+                <div className="flex w-full relative justify-center items-center">
+                    {/* <img
+                        src={currentImage}
+                        alt="Main Image"
+                        className="object-contain max-h-[850px] max-w-full  p-1 "
+                    /> */}
+                    <Image src={painting.image_url} width={painting.width} height={painting.height} onClick={handleOpen} alt="Main Image" />
+
+                    {/* 왼쪽 버튼 */}
                     <button
-                        onClick={handleClose}
-                        className="absolute top-4 right-4 text-white text-3xl"
+                        onClick={goToPrevious}
+                        className="absolute left-4 top-1/2 transform -translate-y-1/2  bg-gray-600 opacity-50 hover:bg-gray-800 text-white p-2 rounded-full"
                     >
-                        &times;
+                        ◀
+                    </button>
+                    {/* 오른쪽 버튼 */}
+                    <button
+                        onClick={goToNext}
+                        className="absolute right-4 top-1/2 transform -translate-y-1/2 bg-gray-600 opacity-50 hover:bg-gray-800 text-white p-2 rounded-full"
+                    >
+                        ▶
                     </button>
                 </div>
-            )}
+                {/* Thumbnail Navigation */}
+                <div className="flex flex-col w-full py-5 pl-2 bg-black rounded-lg text-gray-200">
+                    <h3 className="text-2xl font-semibold "> {painting.title}</h3>
+                    <p className="text-sm"> by {painting.artist.name}</p>
+                </div>
+
+            </section>
+            {isOpen && <Modal onClose={handleClose}>
+                <PaintingDetailView painting={painting}/>
+            </Modal>}
         </div>
     );
 };
