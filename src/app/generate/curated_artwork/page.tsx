512bbf4257cd26f5c81ec2362d55cb9eccc88c60
armcortex3445 <wlghks5966@naver.com>
2025-05-28 16:24:22 +0900

    feat(app) : prevent to access not implemented route

--- src/app/generate/curated_artwork/page.tsx
+++ src/app/generate/curated_artwork/page.tsx
@@ -1,6 +1,10 @@
 import CuratedArtworkSection from '@/components/generate/CuratedArtworkSection';
+import ErrorModal from '../../../components/modal/ErrorModal';
 
 function CuratedArtwork() {
+
+    return <ErrorModal message='invalid access' />;
+
     return (
         <div className="min-h-screen bg-gray-50 py-8">
             <h1 className="text-3xl font-bold text-center text-gray-800 mb-8">Artwork of week</h1>
