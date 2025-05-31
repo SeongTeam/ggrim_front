44fa877fdcf5f9a1940f43898f7872bb28effefb
armcortex3445 <wlghks5966@naver.com>
2025-05-31 13:53:04 +0900

    chore(next.js) : delete console when production

--- next.config.mjs
+++ next.config.mjs
@@ -21,4 +21,12 @@ const nextConfig = {
       },
 };
 
+if (process.env.NODE_ENV === "production") {
+    nextConfig.compiler = {
+      removeConsole: {
+        exclude: ["error", "warn"],
+      },
+    };
+}
+
 export default nextConfig;
