0acf9d149375d5e663a894a29b683e332501d59d
armcortex3445 <wlghks5966@naver.com>
2025-05-31 16:09:29 +0900

    chore(next) : config alias path @
    - make clear @ alias path as './src'
    - fix next build fail by Module not found when @ alias path is used in importing module

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
 /** @type {import('next').NextConfig} */
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const nextConfig = {
     reactStrictMode: true,
@@ -17,6 +24,7 @@ const nextConfig = {
         if (dev) {
           config.devtool = 'eval-source-map'
         }
+        config.resolve.alias['@'] = path.resolve(__dirname, './src');
         return config
       },
 };
