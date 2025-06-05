5f3a4805127296308e6079cc0c126d7352816dbc
armcortex3445 <wlghks5966@naver.com>
2025-06-05 15:58:48 +0900

    feat(app) : add route handler for log
    - provide middleware.ts to use winston logger which middleware.ts can't import because of edge-runtime

--- src/util/middlewareUtils.ts
+++ src/util/middlewareUtils.ts
@@ -2,6 +2,7 @@ import { headers } from 'next/headers';
 
 export const ENUM_HEADER_LOG = {
     REQUEST_ID: 'x-request-id',
+    INTERNAL_API_KEY: 'x-internal-api-key',
 } as const;
 
 export function getRequestId(): string | undefined {
