b5285d8027ff4d236136d35a9a1fe8bd484b2046
armcortex3445 <wlghks5966@naver.com>
2025-06-16 20:19:22 +0900

    refactor(src) : delete unused folder
    - delete context folder
    - delete mock folder
    - delete types folder

--- src/mock/data/mockCldData.ts
+++
@@ -1,6 +0,0 @@
-export const mockCldData = {
-    threeDogsImagePublicId: 'samples/animals/three-dogs',
-    sheepImagePublicId: 'samples/sheep',
-    sampleImagePublicId: 'cld-sample-3',
-    bochiImagePublicId: 'quests/[2024]/o3l5azkppovl1ndbhpwr',
-};
