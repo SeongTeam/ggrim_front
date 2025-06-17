d6e48222d44f08157c25ce8fbf92ac1c36431b41
armcortex3445 <wlghks5966@naver.com>
2025-06-17 12:58:22 +0900

    refactor(components) : move <ScrollExpander /> definition

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 // import { Navbar } from '@/components';
 import { ArtworkCarousel } from '../components/home/artworkCarousel';
 import ScrollTriggerNavigator from '../components/quiz/ScrollTriggerNavigator';
-import ScrollExpander from '../components/ScrollExpander';
+import ScrollExpander from '../components/home/ScrollExpander';
 import {  getWeekArtWorkDataAction } from '../server-action/backend/painting/api';
 import {  getQuizListAction, scheduleQuizAction, } from '../server-action/backend/quiz/api';
 import { isHttpException, isServerActionError } from '../server-action/backend/util';
