999e01f9e35d3faf0a06bed74ff4bc5876a13c68
armcortex3445 <wlghks5966@naver.com>
2026-04-18 15:24:21 +0900

    feat(state): add reset logic

--- src/state/global/searchKeyword.ts
+++ src/state/global/searchKeyword.ts
@@ -6,13 +6,19 @@ export type SearchKeyWordState = {
 
 export type SearchKeyWordAction = {
 	setKeyword: (keyword: string) => void;
+	resetKeyword: () => void;
 };
 
 export type SearchKeyWordStore = SearchKeyWordState & SearchKeyWordAction;
 
-export const createSearchKeyWordStore = (initState: SearchKeyWordState = { keyword: "" }) => {
+const defaultKeyword = "";
+
+export const createSearchKeyWordStore = (
+	initState: SearchKeyWordState = { keyword: defaultKeyword },
+) => {
 	return createStore<SearchKeyWordStore>()((set) => ({
 		...initState,
 		setKeyword: (keyword: string) => set({ keyword }),
+		resetKeyword: () => set({ keyword: defaultKeyword }),
 	}));
 };
